2b016b654e09d5b14675d3fd1a883e8d25bdc5e4
julia-dm <yulia.2508dmitruk@gmail.com>
2026-03-06 13:12:50 +0100

    exercise 17 done

---
+++ Stagiaire/Yuliia/17-boucle-foreach.php
@@ -0,0 +1,7 @@
+<?php 
+$prenoms = ["Ines", "Badr","Yuliia","Bryan","Soufiane","Laetitia" ,"Meidhy","Raphaël","Robin","Jean"];
+
+foreach ($prenoms as $prenom) {
+    echo "<li> $prenom</li>";
+}
+?>
