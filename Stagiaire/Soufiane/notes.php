2ed9cbe42205a43d3f92d6081ed9e57e4216dbd5
soufianebouazza-hub <soufiane.bouazza1998@gmail.com>
2026-03-16 10:04:35 +0100

    notes 2.0

--- Stagiaire/Soufiane/notes.php
+++ Stagiaire/Soufiane/notes.php
@@ -11,7 +11,7 @@
     <p>Créez 15-conditions.php : générez un chiffre entre 0 et 10, puis affichez : 0-3 → "X : Nul, étudie la prochaine fois", 4-5 → "X : Peut mieux faire", 6-7 → "X : Bien", 8-10 → "X : Très bien".</p>
     <?php
     $random = rand(0,10);
-    if($random === 0){
+    if($random <= 3){
         echo "$random : Nul, étudie la prochaine fois";
     }
     elseif($random<=5){
